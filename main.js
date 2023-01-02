2dc01b0cc016049f7f6465ffa51ef81fa05fd278
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-01-02 16:49:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,45 @@ function modelReady()
 {
   classifier.classify(gotResults);
 }
+
+function gotResults(error, results)
+{
+  if (error)
+  {
+    console.error(error);
+  }
+  else
+  {
+    console.log(results);
+
+    var randomNumeberR = Math.floor(Math.random() * 255) + 1;
+    var randomNumeberB = Math.floor(Math.random() * 255) + 1;
+    var randomNumeberG = Math.floor(Math.random() * 255) + 1; 
+
+    document.getElementById("h3animal").innerHTML = 'Posso ouvir - ' + results[0].label;
+    document.getElementById("h3animal").style.color = "rgb(" + randomNumeberR + "," + randomNumeberG + "," + randomNumeberB +")";
+  
+    img = document.getElementById("img1");
+
+    if (results[0].label == "Latido")
+    {
+      img.src = "bark.gif";
+    }
+    
+    else if (results[0].label == "Miado")
+    {
+      img.src = "lero.png";
+    }
+    
+    else if (results[0].label == "Silvar")
+    {
+      img.src = "folu.jpg";
+    }
+    
+    else
+    {
+      img.src = "listen.gif";
+    }
+  }
+}
+
